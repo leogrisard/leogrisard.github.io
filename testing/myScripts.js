33a45d5ed171c9882b9b3eb48a2627f40c66dd68
leogrisard <54583286+leogrisard@users.noreply.github.com>
2020-01-24 07:58:11 -0500

    Update myScripts.js

--- testing/myScripts.js
+++ testing/myScripts.js
@@ -1,4 +1,20 @@
-for(var x = 0; x <= 8; x++){
-  var y = Math.pow(2,x);
-  document.write("number " + y + "<br />");
+function myLoop() {
+  var start = document.getElementById("start").value;
+  var end = document.getElementById("end").value;
+  var count = document.getElementById("count").value;
+  
+  count = Number(count);
+  start = Number(start);
+  end = Number(end);
+  
+  console.log(typeof count);
+  console.log(typeof start);
+  console.log(typeof end);
+    
+  for (var i = start; i <= end; i+=count) 
+    {
+      document.write( '<p>' + i + '</p>');
+      // document.write(jump);
+      // console.log(i);
+    }
 }
